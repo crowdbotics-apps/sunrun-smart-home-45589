5b64a3408653b052b4f463413f9cd4a1d6c626d1
Crowdbotics <team@crowdbotics.com>
2024-01-26 19:18:38 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sunrun-smart-home-45589",
+  url: "https://sunrun-smart-home-45589.botics.co",
+  api: "https://sunrun-smart-home-45589.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
